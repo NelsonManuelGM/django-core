df790b188f1f4877330a7ac8a7b2b1fb62349708
Nelson Gonzalez <nelsitogm25@gmail.com>
2020-05-31 21:17:55 -0400

    Updating test_core

--- appcore/tests/test_core.py
+++ appcore/tests/test_core.py
@@ -1,7 +1,7 @@
 """TestCore configuration"""
 import inject
 from django.test import TestCase
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 
 from djcore.inject import config_inject
 
@@ -9,14 +9,19 @@ from djcore.inject import config_inject
 class TestCaseCore(TestCase):
     """Parent class for system tests"""
 
+    class Meta:
+        abstract = True
+
     def setUp(self) -> None:
         super().setUp()
         inject.clear_and_configure(config_inject)
 
 
-class APITestCaseCore(APITestCase):
+class APITestCaseCore(APITestCase, TestCase):
     """Parent class for system tests"""
+    class Meta:
+        abstract = True
 
     def setUp(self) -> None:
         inject.clear_and_configure(config_inject)
-
+        self.client = APIClient()
